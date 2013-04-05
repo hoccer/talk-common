9ecfb893cbda110e3af65792f4d348665621a1b7
Ingo Albrecht <prom@berlin.ccc.de>
2013-04-05 13:44:47 +0200

    Implement first relationship manipulation / token pairing

--- src/main/java/com/hoccer/talk/model/TalkRelationship.java
+++ src/main/java/com/hoccer/talk/model/TalkRelationship.java
@@ -6,6 +6,19 @@ public class TalkRelationship {
     public static final String STATE_FRIEND  = "friend";
     public static final String STATE_BLOCKED = "blocked";
 
+    public static boolean isValidState(String state) {
+        if(state.equals(STATE_NONE)) {
+            return true;
+        }
+        if(state.equals(STATE_FRIEND)) {
+            return true;
+        }
+        if(state.equals(STATE_BLOCKED)) {
+            return true;
+        }
+        return false;
+    }
+
     private String _id;
 
     String clientId;

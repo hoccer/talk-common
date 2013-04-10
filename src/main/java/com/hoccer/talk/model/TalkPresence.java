e24a4a441dfad9b1973ee8122c470444f4d07a3d
Ingo Albrecht <prom@berlin.ccc.de>
2013-04-10 16:47:22 +0200

    First presence data structure

--- src/main/java/com/hoccer/talk/model/TalkPresence.java
+++ src/main/java/com/hoccer/talk/model/TalkPresence.java
@@ -1,4 +1,40 @@
 package com.hoccer.talk.model;
 
 public class TalkPresence {
+
+    private String _id;
+
+    String clientId;
+
+    String clientName;
+
+    String clientStatus;
+
+    public TalkPresence() {
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientStatus() {
+        return clientStatus;
+    }
+
+    public void setClientStatus(String clientStatus) {
+        this.clientStatus = clientStatus;
+    }
+
 }

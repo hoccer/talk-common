fb6f51e93a1494c6005ecddd81eba9dd68bfe13c
Ingo Albrecht <prom@berlin.ccc.de>
2013-04-16 01:28:40 +0200

    Comments for client protocol

--- src/main/java/com/hoccer/talk/rpc/ITalkRpcClient.java
+++ src/main/java/com/hoccer/talk/rpc/ITalkRpcClient.java
@@ -18,15 +18,34 @@ import com.hoccer.talk.model.TalkPresence;
  */
 public interface ITalkRpcClient {
 
+    /**
+     * Sent on login if the client has no push config
+     */
     @JsonRpcNotification
     void pushNotRegistered();
 
+    /**
+     * Sent to deliver a message from another client
+     *
+     * @param d delivery object for M towards this client
+     * @param m message to be delivered
+     */
 	@JsonRpcNotification
 	void incomingDelivery(TalkDelivery d, TalkMessage m);
-	
+
+    /**
+     * Sent to notify of a state change on an outgoing delivery
+     *
+     * @param d delivery object that has changed state
+     */
 	@JsonRpcNotification
 	void outgoingDelivery(TalkDelivery d);
 
+    /**
+     * Sent to notify of a presence update
+     *
+     * @param presence that has changed
+     */
     @JsonRpcNotification
     void presenceUpdated(TalkPresence presence);
 	

7785ff27d307c30720e20afc474f68b65cd30bf5
Ingo Albrecht <prom@berlin.ccc.de>
2013-06-26 18:12:32 +0200

    Added missing fields to client info

--- src/main/java/com/hoccer/talk/model/TalkClientInfo.java
+++ src/main/java/com/hoccer/talk/model/TalkClientInfo.java
@@ -6,6 +6,16 @@ public class TalkClientInfo {
 
     Date clientTime;
 
+    String clientName;
+    String clientVersion;
+    String clientLanguage;
+
+    String systemName;
+    String systemVersion;
+    String systemLanguage;
+
+    String deviceModel;
+
     public TalkClientInfo() {
     }
 
@@ -15,6 +25,62 @@ public class TalkClientInfo {
 
     public void setClientTime(Date clientTime) {
         this.clientTime = clientTime;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientVersion() {
+        return clientVersion;
+    }
+
+    public void setClientVersion(String clientVersion) {
+        this.clientVersion = clientVersion;
+    }
+
+    public String getClientLanguage() {
+        return clientLanguage;
+    }
+
+    public void setClientLanguage(String clientLanguage) {
+        this.clientLanguage = clientLanguage;
+    }
+
+    public String getSystemName() {
+        return systemName;
+    }
+
+    public void setSystemName(String systemName) {
+        this.systemName = systemName;
+    }
+
+    public String getSystemVersion() {
+        return systemVersion;
+    }
+
+    public void setSystemVersion(String systemVersion) {
+        this.systemVersion = systemVersion;
+    }
+
+    public String getSystemLanguage() {
+        return systemLanguage;
+    }
+
+    public void setSystemLanguage(String systemLanguage) {
+        this.systemLanguage = systemLanguage;
+    }
+
+    public String getDeviceModel() {
+        return deviceModel;
+    }
+
+    public void setDeviceModel(String deviceModel) {
+        this.deviceModel = deviceModel;
     }
 
 }

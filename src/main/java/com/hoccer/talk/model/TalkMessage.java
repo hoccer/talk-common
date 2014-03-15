0784332f7e7a3f786e12bff4fc4d6e3597815df5
Pavel Mayer <pavel@hoccer.com>
2014-03-15 22:43:46 +0100

    - added message hmac sending

--- src/main/java/com/hoccer/talk/model/TalkMessage.java
+++ src/main/java/com/hoccer/talk/model/TalkMessage.java
@@ -1,8 +1,19 @@
 package com.hoccer.talk.model;
 
+import com.hoccer.talk.crypto.AESCryptor;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
+import org.apache.commons.codec.binary.Base64;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.UUID;
 
@@ -66,7 +77,46 @@ public class TalkMessage {
     public TalkMessage() {
     }
 
-	public String getMessageId() {
+    // computes SHA256-Hmac - Message must be already encrypted or not yet decrypted
+    public byte[] computeHMAC()  {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA256");
+            Charset utf8 = Charset.forName("UTF-8");
+
+            // messageTag
+            // md.update(getMessageTag().getBytes(utf8));
+
+            // senderId
+            md.update(getSenderId().getBytes(utf8));
+
+            // timeSent
+            String timeSent = String.valueOf(getTimeSent().getTime());
+            System.out.println("timeSent=" +timeSent);
+            md.update(timeSent.getBytes(utf8));
+
+            // body
+            md.update(getBody().getBytes(utf8));
+
+            // attachment
+            if(getAttachment() != null) {
+                md.update(getAttachment().getBytes(utf8));
+                md.update(getAttachmentFileId().getBytes(utf8));
+            }
+            // salt
+            if (getSalt() != null && getSalt().length()>0) {
+                md.update(getSalt().getBytes(utf8));
+            }
+
+            byte[] digest = md.digest();
+            return digest;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public String getMessageId() {
 		return messageId;
 	}
 

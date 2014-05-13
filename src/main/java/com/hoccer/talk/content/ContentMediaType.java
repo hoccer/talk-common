16d8f642602c893a717d44aeb6f629b5dd0547ab
Nico Nußbaum <nico@artcom.de>
2014-05-13 16:03:33 +0200

    Merge branch 'feature/android_musicplayer' of github.com:hoccer/hoccer-talk-spike into feature/android_musicplayer
    Conflicts:
    	android/src/main/java/com/hoccer/xo/android/fragment/AudioAttachmentListFragment.java
    	android/src/main/java/com/hoccer/xo/android/service/MediaPlayerService.java

--- src/main/java/com/hoccer/talk/content/ContentMediaType.java
+++ src/main/java/com/hoccer/talk/content/ContentMediaType.java
@@ -4,4 +4,13 @@ package com.hoccer.talk.content;
  * Created by nico on 13/05/2014.
  */
 public class ContentMediaType {
+
+    public static final String UNKNOWN = "Unknown file";
+    public static final String IMAGE = "image";
+    public static final String AUDIO = "audio";
+    public static final String VIDEO = "video";
+    public static final String CONTACT ="contact";
+    public static final String LOCATION = "location";
+    public static final String DATA = "data";
+
 }

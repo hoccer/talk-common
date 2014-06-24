d33da471f45bda16f59d463ca59f5f8a48878689
Andreas Marr <andreasm@artcom.de>
2014-06-24 15:20:20 +0200

    Added a model for the server-side representation of the Client info. This stores the information from client transmitted via hello().
    Storing this information would allow selecting specific clients e.g. for purposes of sending system messages but also for purely informational (dashboard) reasons.
    
    Currently the assumption is that one client is only ever running on one physical client. The abstraction that a TalkClient can be run simultaneously on several physical devices is not reflected.
    
    This is probably ok this way since multi-device support may function entirely differently in the future by means of channels, where one channel is the abstract representation of what currently is TalkClient.

---
+++ src/main/java/com/hoccer/talk/model/TalkClientHostInfo.java
@@ -0,0 +1,145 @@
+package com.hoccer.talk.model;
+
+import java.util.Date;
+
+/**
+ * A server-side container for all the information transfered via the hello() rpc call
+ */
+public class TalkClientHostInfo {
+
+    /** the time on the client when the hello call was made */
+    Date clientTime;
+
+    /** The name of the client program; on iOS it is the name of the app 'Hoccer XO'*/
+    String clientName;
+
+    /** A version string for the client program */
+    String clientVersion;
+
+    /** A build number of the client program; on iOS it is the build number */
+    int clientBuildNumber;
+
+    /** A canonicalized IETF BCP 47 language identifier for the current client UI language, e.g. 'de' for german */
+    String clientLanguage;
+
+    /** Name of the operating system, e.g. 'iPhone OS' */
+    String systemName;
+
+    /** Version of the operating system, e.g. '7.0.4' */
+    String systemVersion;
+
+    /** A canonicalized IETF BCP 47 language identifier for the current system language , e.g. 'de' for german */
+    String systemLanguage;
+
+    /** A hardware device name identifier, e.g. 'iPhone6,1' */
+    String deviceModel;
+
+    /** A string to control the support mode for this device; 'log' will activate additional logging on the server fpr this connection */
+    String supportTag;
+
+    /** clientCrashed is set to 1 if the client has crashed since the last connection */
+    // boolean clientCrashed;
+
+    /** hasUpdated is set to 1 if the client has been updated the last connection */
+    // boolean hasUpdated;
+
+    /** uncleanShutdown is set to 1 if last connection has been terminated with an error on the client side */
+    // boolean uncleanShutdown;
+
+    String clientId;
+
+    public TalkClientHostInfo() {
+    }
+
+    public TalkClientHostInfo(TalkClientInfo clientInfo) {
+
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public Date getClientTime() {
+        return clientTime;
+    }
+
+    public void setClientTime(Date clientTime) {
+        this.clientTime = clientTime;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientVersion() {
+        return clientVersion;
+    }
+
+    public void setClientVersion(String clientVersion) {
+        this.clientVersion = clientVersion;
+    }
+
+    public int getClientBuildNumber() {
+        return clientBuildNumber;
+    }
+
+    public void setClientBuildNumber(int clientBuildNumber) {
+        this.clientBuildNumber = clientBuildNumber;
+    }
+
+    public String getClientLanguage() {
+        return clientLanguage;
+    }
+
+    public void setClientLanguage(String clientLanguage) {
+        this.clientLanguage = clientLanguage;
+    }
+
+    public String getSystemName() {
+        return systemName;
+    }
+
+    public void setSystemName(String systemName) {
+        this.systemName = systemName;
+    }
+
+    public String getSystemVersion() {
+        return systemVersion;
+    }
+
+    public void setSystemVersion(String systemVersion) {
+        this.systemVersion = systemVersion;
+    }
+
+    public String getSystemLanguage() {
+        return systemLanguage;
+    }
+
+    public void setSystemLanguage(String systemLanguage) {
+        this.systemLanguage = systemLanguage;
+    }
+
+    public String getDeviceModel() {
+        return deviceModel;
+    }
+
+    public void setDeviceModel(String deviceModel) {
+        this.deviceModel = deviceModel;
+    }
+
+    public String getSupportTag() {
+        return supportTag;
+    }
+
+    public void setSupportTag(String supportTag) {
+        this.supportTag = supportTag;
+    }
+}
